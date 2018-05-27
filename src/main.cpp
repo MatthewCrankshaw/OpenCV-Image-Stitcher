10b8e6e80871f2f194f24885d4a3c26aaed149ad
Accusmus <mhcrankshaw2@gmail.com>
2018-05-28 11:39:06 +1200

    Feature detection working

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/features2d.hpp>
+#include <opencv2/xfeatures2d.hpp>
 
 using namespace std;
+using namespace cv;
+using namespace cv::xfeatures2d;
 
 int main(int argc, char ** argv)
 {
@@ -20,14 +24,31 @@ int main(int argc, char ** argv)
 
     cv::Mat img_1 = imread( argv[1], cv::IMREAD_GRAYSCALE );
     cv::Mat img_2 = imread( argv[2], cv::IMREAD_GRAYSCALE );
+    resize(img_1, img_1, Size(480, 640), 0,0, INTER_LINEAR);
+    resize(img_2,img_2, Size(480, 640), 0, 0, INTER_LINEAR);
 
     if(!img_1.data || !img_2.data){
         cout << "Error reading images!" << endl;
         return 1;
     }
 
-    cv::imshow("Img1", img_1);
-    cv::imshow("Img2", img_2);
+    int minHessian = 600;
+
+    cv::Ptr<SURF> detector = SURF::create(minHessian);
+    vector<KeyPoint> keypoints_1, keypoints_2;
+
+    detector->detect(img_1, keypoints_1);
+    detector->detect(img_2, keypoints_2);
+
+    Mat img_keypoints_1;
+    Mat img_keypoints_2;
+
+    drawKeypoints( img_1, keypoints_1, img_keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+    drawKeypoints( img_2, keypoints_2, img_keypoints_2, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+
+
+    cv::imshow("Img1", img_keypoints_1);
+    cv::imshow("Img2", img_keypoints_2);
 
     cv::waitKey(0);
     return 0;
