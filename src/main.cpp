0574b13cefa0d95c07b64cad512100ef5937da01
Accusmus <mhcrankshaw2@gmail.com>
2018-05-28 12:28:29 +1200

    feature matching working with ORB

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ using namespace std;
 using namespace cv;
 using namespace cv::xfeatures2d;
 
+#define WINDOW_SIZE 600
+
 int main(int argc, char ** argv)
 {
 
@@ -16,29 +18,37 @@ int main(int argc, char ** argv)
         cout << "Please provide 2 images as arguements" << endl;
         return 1;
     }
+
+    Mat original_1, original_2;
+    Mat img_1, img_2;
+
     cv::namedWindow("Img1", 0);
     cv::namedWindow("Img2", 0);
+    cv::namedWindow("matches", 0);
 
-    cv::resizeWindow("Img1", 500, 500);
-    cv::resizeWindow("Img2", 500, 500);
+    cv::resizeWindow("Img1", WINDOW_SIZE, WINDOW_SIZE);
+    cv::resizeWindow("Img2", WINDOW_SIZE, WINDOW_SIZE);
+    cv::resizeWindow("matches", WINDOW_SIZE, WINDOW_SIZE);
 
-    cv::Mat img_1 = imread( argv[1], cv::IMREAD_GRAYSCALE );
-    cv::Mat img_2 = imread( argv[2], cv::IMREAD_GRAYSCALE );
-    resize(img_1, img_1, Size(480, 640), 0,0, INTER_LINEAR);
-    resize(img_2,img_2, Size(480, 640), 0, 0, INTER_LINEAR);
-
-    if(!img_1.data || !img_2.data){
+    original_1 = imread( argv[1], IMREAD_COLOR );
+    original_2 = imread( argv[2], IMREAD_COLOR );
+    if(!original_1.data || !original_2.data){
         cout << "Error reading images!" << endl;
         return 1;
     }
+    resize(original_1, original_1, Size(720, 1280), 0,0, INTER_LINEAR);
+    resize(original_2, original_2, Size(720, 1280), 0, 0, INTER_LINEAR);
 
-    int minHessian = 600;
+    cvtColor(original_1, img_1, CV_BGR2GRAY);
+    cvtColor(original_2, img_2, CV_BGR2GRAY);
 
-    cv::Ptr<SURF> detector = SURF::create(minHessian);
+    int minHessian = 200;
+    cv::Ptr<ORB> detector = ORB::create(minHessian);
     vector<KeyPoint> keypoints_1, keypoints_2;
 
-    detector->detect(img_1, keypoints_1);
-    detector->detect(img_2, keypoints_2);
+    Mat descriptors1, descriptors2;
+    detector->detectAndCompute(img_1, Mat(), keypoints_1, descriptors1);
+    detector->detectAndCompute(img_2, Mat(), keypoints_2, descriptors2);
 
     Mat img_keypoints_1;
     Mat img_keypoints_2;
@@ -46,9 +56,23 @@ int main(int argc, char ** argv)
     drawKeypoints( img_1, keypoints_1, img_keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
     drawKeypoints( img_2, keypoints_2, img_keypoints_2, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
 
+    vector<DMatch> matches;
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
+    matcher->match(descriptors1, descriptors2, matches, Mat());
+
+    Mat matchesImage;
+    drawMatches(original_1, keypoints_1, original_2, keypoints_2, matches, matchesImage);
+
+    sort(matches.begin(), matches.end());
+
+    const int goodMatches = matches.size() * 0.15f;
+    matches.erase(matches.begin()+goodMatches, matches.end());
+
+    imwrite("ORB_matcher.jpg", matchesImage);
 
     cv::imshow("Img1", img_keypoints_1);
     cv::imshow("Img2", img_keypoints_2);
+    cv::imshow("matches", matchesImage);
 
     cv::waitKey(0);
     return 0;
