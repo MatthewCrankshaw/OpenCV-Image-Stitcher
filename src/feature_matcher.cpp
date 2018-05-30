c44a7a5488cbd38d3b280a26a865c432ee2f5441
Accusmus <mhcrankshaw2@gmail.com>
2018-05-31 11:01:15 +1200

    made a few improvements

--- src/feature_matcher.cpp
+++ src/feature_matcher.cpp
@@ -6,8 +6,8 @@ using namespace cv::xfeatures2d;
 
 feature_matcher::feature_matcher()
 {
-    imgWidth = 640;
-    imgHeight = 480;
+    imgWidth = 720;
+    imgHeight = 1280;
 }
 
 feature_matcher::~feature_matcher()
@@ -25,7 +25,7 @@ void feature_matcher::getHomographySURF(const Mat &src1, const Mat &src2, Mat &m
     cvtColor(resize1, gray1, CV_BGR2GRAY);
     cvtColor(resize2, gray2, CV_BGR2GRAY);
 
-    Ptr<SURF> surf = SURF::create(500);
+    Ptr<SURF> surf = SURF::create(600);
     surf->detectAndCompute(gray1, Mat(), keypoints1, descriptors1);
     surf->detectAndCompute(gray2, Mat(), keypoints2, descriptors2);
 
@@ -77,7 +77,7 @@ void feature_matcher::getHomographyORB(const Mat &src1, const Mat &src2, Mat &mt
     cvtColor(resize1, gray1, CV_BGR2GRAY);
     cvtColor(resize2, gray2, CV_BGR2GRAY);
 
-    int minHessian = 500;
+    int minHessian = 600;
     Ptr<ORB> detector = ORB::create(minHessian);
     detector->detectAndCompute(gray1, Mat(), keypoints1, descriptors1);
     detector->detectAndCompute(gray2, Mat(), keypoints2, descriptors2);
@@ -85,12 +85,20 @@ void feature_matcher::getHomographyORB(const Mat &src1, const Mat &src2, Mat &mt
     vector<DMatch> matches;
     Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
     matcher->match(descriptors1, descriptors2, matches, Mat());
-
+//
     sort(matches.begin(), matches.end());
 
-    const int goodMatches = matches.size() * 0.15f;
+    const int goodMatches = matches.size() * 0.10f;
     matches.erase(matches.begin()+goodMatches, matches.end());
 
     drawMatches(resize1, keypoints1, resize2, keypoints2, matches, mtchs);
+
+    vector<Point2f> points1, points2;
+    for(size_t i = 0; i < matches.size(); i++){
+        points1.push_back(keypoints1[matches[i].queryIdx].pt);
+        points2.push_back(keypoints2[matches[i].trainIdx].pt);
+    }
+
+    h = findHomography(points1, points2, RANSAC);
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,15 @@ int main(int argc, char ** argv)
 
     Mat original_1, original_2;
     Mat matches;
-    Mat h;
+    Mat h1, h2;
 
     cv::namedWindow("matches", 0);
+    cv::namedWindow("w1", 0);
+    cv::namedWindow("w2", 0);
 
     cv::resizeWindow("matches", WINDOW_SIZE, WINDOW_SIZE);
+    cv::resizeWindow("w1", WINDOW_SIZE, WINDOW_SIZE);
+    cv::resizeWindow("w2", WINDOW_SIZE, WINDOW_SIZE);
     original_1 = imread( argv[1], IMREAD_COLOR );
 
     original_2 = imread( argv[2], IMREAD_COLOR );
@@ -38,10 +42,29 @@ int main(int argc, char ** argv)
 
 
     feature_matcher matcher = feature_matcher();
-    matcher.getHomographyORB(original_1, original_2, matches, h);
+    matcher.getHomographyORB(original_1, original_2, matches, h1);
+    //matcher.getHomographySURF(original_2, original_1, matches, h2);
 
+    //warpPerspective(original_1, original_1, h1, Size(original_1.cols*2, original_1.rows*2));
+    //warpPerspective(original_2, original_2, h2, Size(original_2.cols*2, original_2.rows));
+
+//    Mat pano;
+//    vector<Mat> imgs;
+//    imgs.push_back(original_1);
+//    imgs.push_back(original_2);
+//
+//    Ptr<Stitcher> stitcher = Stitcher::create(Stitcher::PANORAMA, true);
+//
+//    Stitcher::Status status = stitcher->stitch(imgs, pano);
+//
+//
+//    cv::imshow("matches", pano);
+//    imwrite("pano.jpg", pano);
+//
     cv::imshow("matches", matches);
-    cv::imwrite("ORB_goodmatches.jpg", matches);
+    cv::imwrite("ORB_matches.jpg", matches);
+    //cv::imshow("w1", original_1);
+    //cv::imshow("w2", original_2);
 
     cv::waitKey(0);
     return 0;
