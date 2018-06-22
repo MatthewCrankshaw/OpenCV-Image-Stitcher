6e3b2e4cf56bc6ca13ee6c96ed706b1cffc70788
Accusmus <mhcrankshaw2@gmail.com>
2018-06-22 13:55:33 +1200

    image stitching working with all feature detectors

--- src/main.cpp
+++ src/main.cpp
@@ -22,10 +22,18 @@ int slider;
 
 vector<Mat> leftImgs, rightImgs, matchedImgs;
 
+vector<Mat> img1, img2, orig1, orig2;
+
 void on_trackbar(int, void *){
-    imshow("imgLeft", leftImgs[slider]);
-    imshow("imgRight", rightImgs[slider]);
-    imshow("matchedImg", matchedImgs[slider]);
+    #if(MODE == 1)
+//    imshow("imgLeft", leftImgs[slider]);
+//    imshow("imgRight", rightImgs[slider]);
+//    imshow("matchedImg", matchedImgs[slider]);
+    #elif(MODE == 2)
+        imshow("window", img1[slider]);
+        imshow("img1", orig1[slider]);
+        imshow("img2", orig2[slider]);
+    #endif // MODE
 }
 
 int main(int argc, char ** argv)
@@ -56,14 +64,14 @@ int main(int argc, char ** argv)
         for(size_t i = 0; i < filenamesLeft.size(); i++){
             Mat temp;
             temp = imread(filenamesLeft[i], 1);
-            leftImgs.push_back(temp);
+            img1.push_back(temp);
             cout << "Loaded Image (" << filenamesLeft[i] << ")" << endl;
         }
 
         for(size_t i = 0; i < filenamesRight.size(); i++){
             Mat temp;
             temp = imread(filenamesRight[i], 1);
-            rightImgs.push_back(temp);
+            img2.push_back(temp);
             cout << "Loaded Image (" << filenamesRight[i] << ")" << endl;
         }
 
@@ -79,7 +87,7 @@ int main(int argc, char ** argv)
 
         for(size_t i = 0; i < filenamesLeft.size(); i++){
             Mat matched, h1;
-            matcher.getHomographySIFT(leftImgs[i], rightImgs[i], 1000, matched, h1);
+            matcher.getHomographySIFT(img1[i], img2[i], 1000, matched, h1);
             matchedImgs.push_back(matched);
             cout << "Detecting features for: " << i << endl;
         }
@@ -98,9 +106,7 @@ int main(int argc, char ** argv)
     }
 
     if(MODE == 2){
-
-        Mat img1, img2, orig1, orig2;
-
+        //setup windows
         namedWindow("window", 0);
         namedWindow("img1", 0);
         namedWindow("img2", 0);
@@ -108,86 +114,139 @@ int main(int argc, char ** argv)
         resizeWindow("img1", Size(600, 600));
         resizeWindow("img2", Size(600, 600));
 
-        orig1 = imread("res/left/9.jpg", 1);
-        orig2 = imread("res/right/9.jpg", 1);
-        if(orig1.data == NULL || orig2.data == NULL){
-            cout << "Error: could not load image" << endl;
-        }
-        resize(orig1, img1, Size(1080, 1920), 0, 0, INTER_NEAREST);
-        resize(orig2, img2, Size(1080, 1920), 0, 0, INTER_NEAREST);
+        vector<String> filenamesRight, filenamesLeft;
+        slider = 0;
 
-        int offsetx = 1000;
-        int offsety = 1000;
+        //get all of the filenames
+        string path1 = "res/left/*", path2 = "res/right/*";
+        glob(path1, filenamesLeft, true);
+        glob(path2, filenamesRight, true);
 
-        Mat reszImg = (Mat_<double>(2,3) << 1, 0, offsetx, 0, 1, offsety);
-        warpAffine(img1, img1, reszImg, Size(3*img1.cols, 2*img1.rows));
-
-        cout << "INFO: Detecting Features" << endl;
-        //Ptr<Feature2D> f2d = xfeatures2d::SIFT::create(100, 3, 0.04, 10, 1.6);
-        //Ptr<Feature2D> f2d = xfeatures2d::SURF::create(1000, 4, 3, false, false);
-        Ptr<Feature2D> f2d = ORB::create(2000, 1.2, 8, 31, 0, 2, 31, 20);
-
-        vector<KeyPoint> keypoints1, keypoints2;
-        f2d->detect(img1, keypoints1);
-        f2d->detect(img2, keypoints2);
-
-        cout << "INFO: Detected Keypoints" << endl;
-
-        Mat descriptors1, descriptors2;
-        f2d->compute(img1, keypoints1, descriptors1);
-        f2d->compute(img2, keypoints2, descriptors2);
-
-        cout << "INFO: Computed descriptors" << endl;
-
-        BFMatcher matcher;
-        vector<DMatch> matches;
-        matcher.match(descriptors1, descriptors2, matches);
-
-        cout << "INFO: Matched descriptors" << endl;
-
-        Mat index;
-        int nbMatch = int(matches.size());
-        Mat tab(nbMatch, 1, CV_32F);
-        for(int i = 0; i < nbMatch; i++){
-            tab.at<float>(i, 0) = matches[i].distance;
-        }
-        sortIdx(tab, index, SORT_EVERY_COLUMN + SORT_ASCENDING);
-        vector<DMatch> bestMatches;
-
-        for(int i = 0; i < 100; i++){
-            bestMatches.push_back(matches[index.at<int>(i,0)]);
+        //check that there are images to load
+        if(filenamesLeft.size() == 0 || filenamesRight.size() == 0){
+            cout << "Files not read" << endl;
+        }else{
+            cout << "Number of left side images: " << filenamesLeft.size() << endl;
+            cout << "Number of right side images: " << filenamesRight.size() << endl;
         }
 
-        cout << "INFO: Computed 200 best matches" << endl;
-
-        vector<Point2f> dst_pts;
-        vector<Point2f> source_pts;
-
-        for(auto it = bestMatches.begin(); it != bestMatches.end(); it++){
-            dst_pts.push_back(keypoints1[it->queryIdx].pt);
-            source_pts.push_back(keypoints2[it->trainIdx].pt);
+        //read in the files and put them in the vector of Mat
+        for(size_t i = 0; i < filenamesLeft.size(); i++){
+            Mat temp;
+            temp = imread(filenamesLeft[i], 1);
+            orig1.push_back(temp);
+            cout << "Loaded Image (" << filenamesLeft[i] << ")" << endl;
         }
 
-        Mat H = findHomography(source_pts, dst_pts, CV_RANSAC);
+        //read in the files and put them in the vector of Mat
+        for(size_t i = 0; i < filenamesRight.size(); i++){
+            Mat temp;
+            temp = imread(filenamesRight[i], 1);
+            orig2.push_back(temp);
+            cout << "Loaded Image (" << filenamesRight[i] << ")" << endl;
+        }
 
-        cout << "INFO: Found Homography: " << H << endl;
+        //check that there is the same number of images on the left and right side
+        if(filenamesLeft.size() != filenamesRight.size()){
+            cout << "not the same number of images in left and right" << endl;
+            exit(1);
+        }
 
-        Mat wim2;
-        warpPerspective(img2, wim2, H, img1.size());
+        //slider max should be the maximum number  of images
+        slider_max =  filenamesLeft.size()-1;
 
-        for(int i = 0; i < img1.cols; i++){
-            for(int j = 0; j < img1.rows; j++){
-                Vec3b colorimg1 = img1.at<Vec3b>(Point(i, j));
-                Vec3b colorimg2 = wim2.at<Vec3b>(Point(i, j));
-                if(norm(colorimg1)==0){
-                    img1.at<Vec3b>(Point(i,j)) = colorimg2;
+        Ptr<Feature2D> f2d = xfeatures2d::SIFT::create(500, 3, 0.04, 10, 1.6);
+        //Ptr<Feature2D> f2d = xfeatures2d::SURF::create(500, 4, 3, false, false);
+        //Ptr<Feature2D> f2d = ORB::create(5000);
+
+        //loop though all images
+        for(int i = 0; i < filenamesLeft.size(); i++){
+
+            //make sure to push back an empty mat otherwise will segment fault
+            img1.push_back(Mat());
+            img2.push_back(Mat());
+            //resize the image for efficiency
+            resize(orig1[i], img1[i], Size(1080, 1920), 0, 0, INTER_NEAREST);
+            resize(orig2[i], img2[i], Size(1080, 1920), 0, 0, INTER_NEAREST);
+
+            //offset that will be used in the resulting images to place it in the center
+            int offsetx = 100;
+            int offsety = 100;
+
+            //resulting images resize
+            Mat reszImg = (Mat_<double>(2,3) << 1, 0, offsetx, 0, 1, offsety);
+            warpAffine(img1[i], img1[i], reszImg, Size(2*img1[i].cols, 1.2*img1[i].rows));
+
+            cout << "INFO: Detected Keypoints for " << i << endl;
+            vector<KeyPoint> keypoints1, keypoints2;
+            f2d->detect(img1[i], keypoints1);
+            f2d->detect(img2[i], keypoints2);
+
+            cout << "INFO: Computed descriptors for " << i << endl;
+            Mat descriptors1, descriptors2;
+            f2d->compute(img1[i], keypoints1, descriptors1);
+            f2d->compute(img2[i], keypoints2, descriptors2);
+
+            cout << "INFO: Matched descriptors for " << i << endl;
+            BFMatcher matcher;
+            vector<DMatch> matches;
+            matcher.match(descriptors1, descriptors2, matches);
+
+            Mat index;
+            int nbMatch = int(matches.size());
+            Mat tab(nbMatch, 1, CV_32F);
+            for(int j = 0; j < nbMatch; j++){
+                tab.at<float>(j, 0) = matches[j].distance;
+            }
+            sortIdx(tab, index, SORT_EVERY_COLUMN + SORT_ASCENDING);
+            vector<DMatch> bestMatches;
+
+            if(matches.size() < 100){
+                cout << "Error: not enough matches for " << i << " Number of matches = " << matches.size() << endl;
+                exit(1);
+            }
+            cout << "Number of matches = " << matches.size() << endl;
+
+            for(int j = 0; j < 100; j++){
+                bestMatches.push_back(matches[index.at<int>(j,0)]);
+            }
+            cout << "INFO: Computed best matches for " << i << endl;
+
+            vector<Point2f> dst_pts;
+            vector<Point2f> source_pts;
+            for(auto it = bestMatches.begin(); it != bestMatches.end(); it++){
+                dst_pts.push_back(keypoints1[it->queryIdx].pt);
+                source_pts.push_back(keypoints2[it->trainIdx].pt);
+            }
+
+            Mat H = findHomography(source_pts, dst_pts, CV_RANSAC);
+            cout << "INFO: Found Homography for " << i << " : " << H << endl;
+
+            Mat wim2;
+            warpPerspective(img2[i], wim2, H, img1[i].size());
+
+            //Nieve aproach at stitching together
+            for(int x = 0; x < img1[i].cols; x++){
+                for(int y = 0; y < img1[i].rows; y++){
+                    Vec3b colorimg1 = img1[i].at<Vec3b>(Point(x, y));
+                    Vec3b colorimg2 = wim2.at<Vec3b>(Point(x, y));
+                    if(norm(colorimg1)==0){
+                        img1[i].at<Vec3b>(Point(x,y)) = colorimg2;
+                    }
                 }
             }
+
         }
 
-        imshow("window", img1);
-        imshow("img1", orig1);
-        imshow("img2", orig2);
+        char trackbarName[50];
+        sprintf(trackbarName, "Image: %d", slider_max);
+
+        createTrackbar(trackbarName, "window", &slider, slider_max, on_trackbar);
+        on_trackbar(slider, 0);
+
+        imshow("img1", orig1[1]);
+        imshow("img2", orig2[1]);
+        imshow("window", img1[1]);
         waitKey(0);
     }
     return 0;
