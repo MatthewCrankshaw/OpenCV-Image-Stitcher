f93cc2554d28c486e039c16f4a83e9274b689236
Matthew Crankshaw <mhcrankshaw2@gmail.com>
2023-02-18 10:10:03 +1300

    removed support for loading multiple images and improved code readability

--- src/main.cpp
+++ src/main.cpp
@@ -11,192 +11,126 @@
 
 enum Mode {FEATURE, STITCH};
 
-Mode mode = STITCH;
-int lastImage;
-int currentImage;
+Mode mode = FEATURE;
 
-std::vector<cv::Mat> leftImgs, rightImgs, matchedImgs;
-std::vector<cv::Mat> imagesLeft, imagesRight;
-std::vector<cv::Mat> originalLeft, originalRight;
+cv::Mat leftImg, rightImg, matchedImg;
+cv::Mat imageLeft, imageRight;
+cv::Mat originalLeft, originalRight;
 
-void handleChangeTrackbar(int, void *);
+void featureMatchMode(cv::String filenameLeft, cv::String filenameRight);
 
-void featureMatchMode(std::vector<cv::String> filenamesLeft, std::vector<cv::String> filenamesRight);
-
-void stitchImagesMode(std::vector<cv::String> filenamesLeft, std::vector<cv::String> filenamesRight);
+void stitchImagesMode(cv::String filenameLeft, cv::String filenameRight);
 
 void createWindow(std::string name, int width = 600, int height = 600);
 
-void loadImages(std::vector<cv::Mat> &images, std::vector<cv::String> filenames);
+void loadImage(cv::Mat &image, cv::String filename);
 
-void createTrackbar(int current, int last);
-
-void stitchImage(int index, cv::Mat homography);
+void stitchImage(cv::Mat homography);
 
 int main(int argc, char ** argv)
 {
-    std::string path1 = "res/left/*", path2 = "res/right/*";
-    std::vector<cv::String> filenamesRight, filenamesLeft;
+    std::string left = "res/left/1.jpg", right = "res/right/1.jpg";
 
-    cv::glob(path1, filenamesLeft, true);
-    cv::glob(path2, filenamesRight, true);
-
-    if(filenamesLeft.size() == 0 || filenamesRight.size() == 0){
-        std::cout << "Files not read" << std::endl;
-    }else{
-        std::cout << "Number of left side images: " << filenamesLeft.size() << std::endl;
-        std::cout << "Number of right side images: " << filenamesRight.size() << std::endl;
-    }
-
-    currentImage = 0;
     if(mode == FEATURE) {
-        featureMatchMode(filenamesLeft, filenamesRight);
+        featureMatchMode(left, right);
     } else if(mode == STITCH) {
-        stitchImagesMode(filenamesLeft, filenamesRight);
+        stitchImagesMode(left, right);
     }
     return 0;
 }
 
-void featureMatchMode(std::vector<cv::String> filenamesLeft, std::vector<cv::String> filenamesRight) {
+void featureMatchMode(cv::String filenameLeft, cv::String filenameRight) {
     createWindow("matchedImg");
 
-    loadImages(imagesLeft, filenamesLeft);
-    loadImages(imagesRight, filenamesRight);
-
-    if(filenamesLeft.size() != filenamesRight.size()){
-        std::cout << "not the same number of images in left and right" << std::endl;
-        exit(1);
-    }
-
-    lastImage =  filenamesLeft.size()-1;
+    loadImage(imageLeft, filenameLeft);
+    loadImage(imageRight, filenameRight);
 
     feature_matcher matcher = feature_matcher(1080, 1920);
 
-    for(size_t i = 0; i < filenamesLeft.size(); i++){
-        cv::Mat matched;
-        matcher.getMatchesSIFT(imagesLeft[i], imagesRight[i], 1000, matched);
-        //matcher.getMatchesORB(img1[i], img2[i], 1000, matched);
-        //matcher.getMatchesSURF(img1[i], img2[i], 10000, matched);
-        matchedImgs.push_back(matched);
-        std::cout << "Detecting features for: " << i << std::endl;
-    }
-
+    matcher.getMatchesSIFT(imageLeft, imageRight, 1000, matchedImg);
     matcher.writeDataFile();
 
-
-    createTrackbar(currentImage, lastImage);
-
+    cv::imshow("matchedImg", matchedImg);
     cv::waitKey(0);
 }
 
-void stitchImagesMode(std::vector<cv::String> filenamesLeft, std::vector<cv::String> filenamesRight) {
+void stitchImagesMode(cv::String filenameLeft, cv::String filenameRight) {
     //setup windows
     createWindow("window");
     createWindow("img1");
     createWindow("img2");
 
-    loadImages(originalLeft, filenamesLeft);
-    loadImages(originalRight, filenamesRight);
+    loadImage(originalLeft, filenameLeft);
+    loadImage(originalRight, filenameRight);
 
-    //check that there is the same number of images on the left and right side
-    if(filenamesLeft.size() != filenamesRight.size()){
-        std::cout << "not the same number of images in left and right" << std::endl;
-        exit(1);
-    }
-
-    //slider max should be the maximum number  of images
-    lastImage = filenamesLeft.size()-1;
-
-    //Ptr<Feature2D> f2d = xfeatures2d::SIFT::create(5000, 3, 0.04, 10, 1.6);
-    //Ptr<Feature2D> f2d = xfeatures2d::SURF::create(1500, 4, 3, false, false);
     cv::Ptr<cv::Feature2D> f2d = cv::ORB::create(10000);
 
-    //loop though all images
-    for(size_t i = 0; i < filenamesLeft.size(); i++){
+    //make sure to push back an empty mat otherwise will segment fault
+    //imagesLeft.push_back(cv::Mat());
+    //imagesRight.push_back(cv::Mat());
+    //resize the image for efficiency
+    resize(originalLeft, imageLeft, cv::Size(1080, 1920), 0, 0, cv::INTER_NEAREST);
+    resize(originalRight, imageRight, cv::Size(1080, 1920), 0, 0, cv::INTER_NEAREST);
 
-        //make sure to push back an empty mat otherwise will segment fault
-        imagesLeft.push_back(cv::Mat());
-        imagesRight.push_back(cv::Mat());
-        //resize the image for efficiency
-        resize(originalLeft[i], imagesLeft[i], cv::Size(1080, 1920), 0, 0, cv::INTER_NEAREST);
-        resize(originalRight[i], imagesRight[i], cv::Size(1080, 1920), 0, 0, cv::INTER_NEAREST);
+    //offset that will be used in the resulting images to place it in the center
+    int offsetx = 100;
+    int offsety = 100;
 
-        //offset that will be used in the resulting images to place it in the center
-        int offsetx = 100;
-        int offsety = 100;
+    //resulting images resize
+    cv::Mat reszImg = (cv::Mat_<double>(2,3) << 1, 0, offsetx, 0, 1, offsety);
+    warpAffine(imageLeft, imageLeft, reszImg, cv::Size(2*imageLeft.cols, 1.2*imageLeft.rows));
 
-        //resulting images resize
-        cv::Mat reszImg = (cv::Mat_<double>(2,3) << 1, 0, offsetx, 0, 1, offsety);
-        warpAffine(imagesLeft[i], imagesLeft[i], reszImg, cv::Size(2*imagesLeft[i].cols, 1.2*imagesLeft[i].rows));
+    std::cout << "INFO: Detected Keypoints " << std::endl;
+    std::vector<cv::KeyPoint> keypoints1, keypoints2;
+    f2d->detect(imageLeft, keypoints1);
+    f2d->detect(imageRight, keypoints2);
 
-        std::cout << "INFO: Detected Keypoints for " << i << std::endl;
-        std::vector<cv::KeyPoint> keypoints1, keypoints2;
-        f2d->detect(imagesLeft[i], keypoints1);
-        f2d->detect(imagesRight[i], keypoints2);
+    std::cout << "INFO: Computed descriptors "<< std::endl;
+    cv::Mat descriptors1, descriptors2;
+    f2d->compute(imageLeft, keypoints1, descriptors1);
+    f2d->compute(imageRight, keypoints2, descriptors2);
 
-        std::cout << "INFO: Computed descriptors for " << i << std::endl;
-        cv::Mat descriptors1, descriptors2;
-        f2d->compute(imagesLeft[i], keypoints1, descriptors1);
-        f2d->compute(imagesRight[i], keypoints2, descriptors2);
+    std::cout << "INFO: Matched descriptors " << std::endl;
+    cv::BFMatcher matcher;
+    std::vector<cv::DMatch> matches;
+    matcher.match(descriptors1, descriptors2, matches);
 
-        std::cout << "INFO: Matched descriptors for " << i << std::endl;
-        cv::BFMatcher matcher;
-        std::vector<cv::DMatch> matches;
-        matcher.match(descriptors1, descriptors2, matches);
+    cv::Mat index;
+    int nbMatch = int(matches.size());
+    cv::Mat tab(nbMatch, 1, CV_32F);
+    for(int j = 0; j < nbMatch; j++){
+        tab.at<float>(j, 0) = matches[j].distance;
+    }
+    sortIdx(tab, index, cv::SORT_EVERY_COLUMN + cv::SORT_ASCENDING);
+    std::vector<cv::DMatch> bestMatches;
 
-        cv::Mat index;
-        int nbMatch = int(matches.size());
-        cv::Mat tab(nbMatch, 1, CV_32F);
-        for(int j = 0; j < nbMatch; j++){
-            tab.at<float>(j, 0) = matches[j].distance;
-        }
-        sortIdx(tab, index, cv::SORT_EVERY_COLUMN + cv::SORT_ASCENDING);
-        std::vector<cv::DMatch> bestMatches;
+    if(matches.size() < 100){
+        std::cout << "Error: not enough matches. Number of matches = " << matches.size() << std::endl;
+        exit(1);
+    }
+    std::cout << "Number of matches = " << matches.size() << std::endl;
 
-        if(matches.size() < 100){
-            std::cout << "Error: not enough matches for " << i << " Number of matches = " << matches.size() << std::endl;
-            exit(1);
-        }
-        std::cout << "Number of matches = " << matches.size() << std::endl;
+    for(int j = 0; j < 100; j++){
+        bestMatches.push_back(matches[index.at<int>(j,0)]);
+    }
+    std::cout << "INFO: Computed best matches" << std::endl;
 
-        for(int j = 0; j < 100; j++){
-            bestMatches.push_back(matches[index.at<int>(j,0)]);
-        }
-        std::cout << "INFO: Computed best matches for " << i << std::endl;
-
-        std::vector<cv::Point2f> dst_pts;
-        std::vector<cv::Point2f> source_pts;
-        for(auto it = bestMatches.begin(); it != bestMatches.end(); it++){
-            dst_pts.push_back(keypoints1[it->queryIdx].pt);
-            source_pts.push_back(keypoints2[it->trainIdx].pt);
-        }
-
-        cv::Mat homography = findHomography(source_pts, dst_pts, cv::RANSAC);
-        std::cout << "INFO: Found Homography for " << i << " : " << homography << std::endl;
-
-        stitchImage(i, homography);
+    std::vector<cv::Point2f> dst_pts;
+    std::vector<cv::Point2f> source_pts;
+    for(auto it = bestMatches.begin(); it != bestMatches.end(); it++){
+        dst_pts.push_back(keypoints1[it->queryIdx].pt);
+        source_pts.push_back(keypoints2[it->trainIdx].pt);
     }
 
-    char trackbarName[50];
-    sprintf(trackbarName, "Image: %d", lastImage);
+    cv::Mat homography = findHomography(source_pts, dst_pts, cv::RANSAC);
+    std::cout << "INFO: Found Homography: " << homography << std::endl;
 
-    cv::createTrackbar(trackbarName, "window", &currentImage, lastImage, handleChangeTrackbar);
-    handleChangeTrackbar(currentImage, 0);
+    stitchImage(homography);
 
-    cv::imshow("img1", originalLeft[1]);
-    cv::imshow("img2", originalRight[1]);
-    cv::imshow("window", imagesLeft[1]);
+    cv::imshow("img1", originalLeft);
+    cv::imshow("img2", originalRight);
+    cv::imshow("window", imageLeft);
     cv::waitKey(0);
-}
-
-void handleChangeTrackbar(int, void *) {
-    if (mode == FEATURE) {
-        imshow("matchedImg", matchedImgs[currentImage]);
-    } else if (mode == STITCH) {
-        cv::imshow("window", imagesLeft[currentImage]);
-        cv::imshow("img1", originalLeft[currentImage]);
-        cv::imshow("img2", originalRight[currentImage]);
-    }
 }
 
 void createWindow(std::string name, int width, int height) {
@@ -204,33 +138,22 @@ void createWindow(std::string name, int width, int height) {
     cv::resizeWindow(name, cv::Size(width, height));
 }
 
-void loadImages(std::vector<cv::Mat> &images, std::vector<cv::String> filenames) {
-    for(size_t i = 0; i < filenames.size(); i++){
-        cv::Mat image = cv::imread(filenames[i], 1);
-        images.push_back(image);
-        std::cout << "Loaded Image (" << filenames[i] << ")" << std::endl;
-    }
+void loadImage(cv::Mat &image, cv::String filename) {
+    image = cv::imread(filename, 1);
+    std::cout << "Loaded Image (" << filename << ")" << std::endl;
 }
 
-void createTrackbar(int current, int last) {
-    char trackbarName[50];
-    sprintf(trackbarName, "Image: %d", lastImage);
-
-    cv::createTrackbar(trackbarName, "imgLeft", &current, lastImage, handleChangeTrackbar);
-    handleChangeTrackbar(current, 0);
-}
-
-void stitchImage(int index, cv::Mat homography) {
+void stitchImage(cv::Mat homography) {
     cv::Mat wim2;
-    warpPerspective(imagesRight[index], wim2, homography, imagesLeft[index].size());
+    warpPerspective(imageRight, wim2, homography, imageLeft.size());
 
     //Nieve aproach at stitching together
-    for(int x = 0; x < imagesLeft[index].cols; x++){
-        for(int y = 0; y < imagesLeft[index].rows; y++){
-            cv::Vec3b colorimg1 = imagesLeft[index].at<cv::Vec3b>(cv::Point(x, y));
+    for(int x = 0; x < imageLeft.cols; x++){
+        for(int y = 0; y < imageLeft.rows; y++){
+            cv::Vec3b colorimg1 = imageLeft.at<cv::Vec3b>(cv::Point(x, y));
             cv::Vec3b colorimg2 = wim2.at<cv::Vec3b>(cv::Point(x, y));
             if(norm(colorimg1)==0){
-                imagesLeft[index].at<cv::Vec3b>(cv::Point(x,y)) = colorimg2;
+                imageLeft.at<cv::Vec3b>(cv::Point(x,y)) = colorimg2;
             }
         }
     }
